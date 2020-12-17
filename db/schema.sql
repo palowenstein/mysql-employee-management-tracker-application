3c6157bafd43809b386d0b15eeda1c6989cf6355
abbotkinneydude <pa@soundtrackspecialist.com>
2020-12-16 23:36:23 -0800

    db folder, schema.sql, seed.sql

---
+++ db/schema.sql
@@ -0,0 +1,52 @@
+-- Drops the EmployeeRoster_db if it already exists
+DROP DATABASE IF EXISTS EmployeeRoster_db;
+
+-- Creates Database EmployeeRoster_db, OK for use.
+CREATE database EmployeeRoster_db;
+USE EmployeeRoster_db;
+
+-- Department Table Specs
+CREATE TABLE department  (
+  id INT NOT NULL AUTO_INCREMENT,
+  dep_name VARCHAR (30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Role Table Specs
+CREATE TABLE roles  (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (10,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Employee Table Specs
+CREATE TABLE employee  (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+
+-- ReadMe Markdown
+
+-- * **department**:
+--   * **id** - INT PRIMARY KEY
+--   * **name** - VARCHAR(30) to hold department name
+
+-- * **role**:
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
+
+-- * **employee**:
+--   * **id** - INT PRIMARY KEY
+--   * **first_name** - VARCHAR(30) to hold employee first name
+--   * **last_name** - VARCHAR(30) to hold employee last name
+--   * **role_id** - INT to hold reference to role employee has
+--   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
